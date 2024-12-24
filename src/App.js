55cd6de5faf6c4a3664daf023454bd61e4f96bc0
Harshit Gupta <harshitgupta0910@gmail.com>
2024-12-24 18:39:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Contacts from "./components/Contacts";
+import History from "./components/History";
+import MainForm from "./components/MainForm";
 
 function App() {
+  const [contactHistory, setContactHistory] = useState([]);
+  const [yourContacts, setYourContacts] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("history")) {
+      setContactHistory(JSON.parse(localStorage.getItem("history")));
+    }
+    if (localStorage.getItem("savedContacts")) {
+      setYourContacts(JSON.parse(localStorage.getItem("savedContacts")));
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <div className="container">
+        <div className="row py-5 my-5 text-center bg-white bg-opacity-50 rounded">
+          <MainForm
+            setContactHistory={setContactHistory}
+            setYourContacts={setYourContacts}
+          />
+          <div className="col-lg-6 py-3">
+            <div className="row">
+              <History
+                contactHistory={contactHistory}
+                setContactHistory={setContactHistory}
+              />
+              <Contacts
+                yourContacts={yourContacts}
+                setYourContacts={setYourContacts}
+                setContactHistory={setContactHistory}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
